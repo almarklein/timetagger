d38cb9f7a7d8fdb4317368168d2675d1324bc129
Almar Klein <almar@almarklein.org>
2021-01-11 17:59:21 +0100

    flake

--- setup.py
+++ setup.py
@@ -15,9 +15,7 @@ setup(
     name="timetagger",
     version=VERSION,
     packages=find_packages(exclude=["tests", "tests.*", "examples", "examples.*"]),
-    package_data={
-        f"timetagger.{x}": ["*"] for x in ["client", "images", "static"]
-    },
+    package_data={f"timetagger.{x}": ["*"] for x in ["client", "images", "static"]},
     python_requires=">=3.6.0",
     install_requires=runtime_deps,
     license="GPL-3.0",
