98a8e4098939be95140b892c266bc1e5135ea955
Almar Klein <almar@almarklein.org>
2021-02-07 22:37:15 +0100

    wip web API

--- timetagger/server/__init__.py
+++ timetagger/server/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 
 from ._utils import asyncthis, asyncify
-from ._apiserver import api_handler, get_user_db, INDICES
+from ._apiserver import api_handler, api_handler_v2, get_user_db, INDICES
 from ._assets import (
     md2html,
     create_assets_from_dir,
