7eaf48ddf089a9f261a10b6b93027be70baf071d
Almar Klein <almar@almarklein.org>
2021-02-25 11:51:49 +0100

    add test to validate hash in SW

--- tests/test_server_assetserver.py
+++ tests/test_server_assetserver.py
@@ -1,3 +1,5 @@
+import sys
+import subprocess
 from pkg_resources import resource_filename
 
 from timetagger.server import create_assets_from_dir
@@ -70,6 +72,45 @@ def test_assets():
             r = p.get(page)
             assert r.status == 404
             assert "404" in r.body.decode() and "not found" in r.body.decode()
+
+
+hash_checker_code = """
+from pkg_resources import resource_filename
+from timetagger.server import create_assets_from_dir, enable_service_worker
+assets = {}
+assets.update(create_assets_from_dir(resource_filename("timetagger.app", ".")))
+assets.update(create_assets_from_dir(resource_filename("timetagger.common", ".")))
+assets.update(create_assets_from_dir(resource_filename("timetagger.images", ".")))
+enable_service_worker(assets)
+cachename = assets["sw.js"].split("currentCacheName =")[1].split("\n")[0]
+print(cachename)
+"""
+
+
+def test_consistent_hash_for_sw():
+    # It's important that the hash is consistent so let's validate this
+
+    oneliner1 = ";".join(hash_checker_code.strip().splitlines())
+    different_code = hash_checker_code.replace("{}", """{"foo": "bar"}""")
+    oneliner2 = ";".join(different_code.strip().splitlines())
+
+    x1 = subprocess.getoutput([sys.executable, "-c", oneliner1])
+    x2 = subprocess.getoutput([sys.executable, "-c", oneliner1])
+    x3 = subprocess.getoutput([sys.executable, "-c", oneliner2])
+
+    x1 = x1.strip().strip(" ';")
+    x2 = x2.strip().strip(" ';")
+    x3 = x3.strip().strip(" ';")
+
+    assert len(x1) > 20
+    assert len(x2) > 20
+    assert len(x3) > 20
+    assert x1.startswith("timetagger")
+    assert x2.startswith("timetagger")
+    assert x3.startswith("timetagger")
+
+    assert x1 == x2
+    assert x1 != x3
 
 
 if __name__ == "__main__":
