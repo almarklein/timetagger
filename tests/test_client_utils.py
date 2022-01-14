f290046cb369f5481b474d35e9ffca249421621d
Almar Klein <almar.klein@gmail.com>
2022-01-14 17:15:21 +0100

    Fixes to startstop time editing (#117)
    * fixes to startstop time editing
    
    * fix test

--- tests/test_client_utils.py
+++ tests/test_client_utils.py
@@ -253,6 +253,11 @@ def test_timestr2tuple():
     assert timestr2tuple("1pm") == (13, 0, 0)
     assert timestr2tuple("11:50 pm") == (23, 50, 0)
 
+    # Out of range
+    assert timestr2tuple("30") == (23, 0, 0)
+    assert timestr2tuple("13:60") == (13, 59, 0)
+    assert timestr2tuple("13:24:60") == (13, 24, 59)
+
 
 if __name__ == "__main__":
     run_tests(globals())
