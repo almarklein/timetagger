5d87d0a07299afffe5674755ecade4ab1080a571
Almar Klein <almar@almarklein.org>
2022-09-20 00:48:52 +0200

    improve sync indicator icons a bit

--- timetagger/app/front.py
+++ timetagger/app/front.py
@@ -1151,12 +1151,12 @@ class TopWidget(Widget):
         M = dict(
             pending="\uf067",  # uf067 uf055
             sync="\uf2f1",
-            ok="\uf560",  # uf560 uf00c
-            warn="\uf12a",
-            error="\uf00d",
+            ok="\uf00c",  # uf560 uf560 uf00c
+            warning="\uf071",
+            error="\uf12a",
         )
         state = window.store.state
-        text = M.get(state, "\uf00c")
+        text = M.get(state, "\uf1eb")
         if text:
             ctx.save()
             try:
