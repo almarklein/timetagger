6ff1ed06a955e1d5d699b4dfdd3d013b2448b3e2
starlight-2003 <sanghuynh1603@gmail.com>
2024-11-13 09:33:56 +0700

    change the local host
    im changing the localhost from 0.0.0.0:80 to 127.0.0.106:5000

--- timetagger/_config.py
+++ timetagger/_config.py
@@ -43,7 +43,7 @@ class Config:
     """
 
     _ITEMS = [
-        ("bind", str, "0.0.0.0:80"),
+        ("bind", str, "127.0.0.106:5000"),
         ("datadir", str, "~/_timetagger"),
         ("log_level", str, "info"),
         ("credentials", str, ""),
