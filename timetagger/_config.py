3e3ac6d9d3f413671539bfb76d1447a78f8821d0
starlight-2003 <sanghuynh1603@gmail.com>
2024-11-13 10:23:37 +0700

    Revert "another bind"
    This reverts commit feb82240d40b7cafc98d32db19f2466f2f613cd3.

--- timetagger/_config.py
+++ timetagger/_config.py
@@ -43,7 +43,7 @@ class Config:
     """
 
     _ITEMS = [
-        ("bind", str, "127.0.0.1:1234"),
+        ("bind", str, "0.0.0.0:80"),
         ("datadir", str, "~/_timetagger"),
         ("log_level", str, "info"),
         ("credentials", str, ""),
